ea24f6b176c5ebc194a95579940b2e19de10f001
oghene-ella <oghenekaro57@gmail.com>
2023-09-18 18:22:49 +0100

    created the homepage and login page

---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,9 @@
+const Homepage = () => {
+  return (
+    <section>
+        <h1>Homepage</h1>
+    </section>
+  )
+}
+
+export default Homepage
---
+++ src/pages/Login.jsx
@@ -0,0 +1,11 @@
+import TheLogin from "../components/authentication/Login/Login"
+
+const Login = () => {
+  return (
+    <section>
+        <TheLogin />
+    </section>
+  )
+}
+
+export default Login
