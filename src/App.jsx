e74f93d67f2f07ef2a64fbbab721b1a1073696e1
oghene-ella <oghenekaro57@gmail.com>
2023-09-18 18:23:14 +0100

    added custom fonts, firebase file and modified the app.js file

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
-function App() {
+import Login from "./pages/Login"
+
+const App = () => {
   return (
     <>
-      <h1 className="bg-purple-800 text-white text-7xl">Hello Ellahh!</h1>
+    <Login/>
+      {/* <h1 className="bg-purple-800 text-white text-7xl">Hello Ellahh!</h1> */}
     </>
   )
 }
---
+++ src/firebase.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+	apiKey: "AIzaSyBElNbXWRtdJFRTVhmCTS0osMfXpF-Qwpw",
+	authDomain: "img-gallery-57191.firebaseapp.com",
+	projectId: "img-gallery-57191",
+	storageBucket: "img-gallery-57191.appspot.com",
+	messagingSenderId: "1014301752187",
+	appId: "1:1014301752187:web:ddb167aaad5e4641f99bde",
+	measurementId: "G-4Z7M4C9ETW",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// Initialize Firebase Authentication and get a reference to the service
+export const auth = getAuth(app);
