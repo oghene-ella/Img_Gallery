53984a8597f479ccd9c5f0f897becd290313aa41
oghene-ella <oghenekaro57@gmail.com>
2023-09-18 18:42:53 +0100

    made the login page responsive

--- src/components/authentication/Login/Login.jsx
+++ src/components/authentication/Login/Login.jsx
@@ -3,14 +3,14 @@ import img_logo from "../../../assets/logo/logo1.png"
 import login_bg from "../../../assets/login_bg.webp";
 const Login = () => {
 	return (
-		<section className="w-screen h-screen flex gap-5 font-mooli_sans">
-			<section className="w-1/2 flex flex-col items-center p-5 justify-center gap-10">
+		<section className="w-screen h-screen flex gap-5 font-mooli_sans justify-center">
+			<section className="sm:w-5/6 lg:w-1/2 flex flex-col items-center p-5 justify-center gap-10">
 				{/* header and logo */}
 				<span className="w-11/12 flex flex-col gap-5">
-					<img src={img_logo} className="w-1/12" />
+					<img src={img_logo} className="w-1/6 sm:w-1/12" />
 					<span className="flex flex-col gap-2">
-						<h1 className="text-5xl">Welcome Back!</h1>
-						<p className="text-lg text-gray-500 w-2/3">
+						<h1 className="text-3xl sm:text-5xl">Welcome Back!</h1>
+						<p className="sm:text-lg text-gray-500 md:w-2/3 ">
 							Explore and create your gallery to building a safe and inspiring
 							space for all our users.
 						</p>
@@ -46,10 +46,10 @@ const Login = () => {
 					</p>
 
                     {/* login button */}
-					<input type="submit" value="Login" className="bg-blue-700 text-white py-3 rounded-xl hover:bg-blue-600" />
+					<input type="submit" value="Login" className="bg-blue-700 text-white py-3 rounded-xl hover:bg-blue-600 text-lg" />
 				</form>
 			</section>
-			<img src={login_bg} className="w-1/2 bg-cover" />
+			<img src={login_bg} className="hidden lg:block lg:w-1/2 bg-cover" />
 		</section>
 	);
 };
