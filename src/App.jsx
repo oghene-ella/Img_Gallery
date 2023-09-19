9e15ed3a7204149359471b0c857273e02c774034
oghene-ella <oghenekaro57@gmail.com>
2023-09-19 20:07:55 +0100

    fixed routing

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,16 @@ import { Routes, Route} from "react-router-dom";
 import Login from "./pages/Login.jsx";
 import Home from "./pages/Homepage.jsx"
 import NotFound from "./pages/NotFound.jsx"
-// import Authentication from "./components/authentication/LoginAuthentication"
+import Authentication from "./components/authentication/LoginAuthentication.jsx"
 
 
 const App = () => {
 	return (
 		<Routes>
 			<Route path="/" element={<Login />} />
-			{/* <Route path="/" element={<Authentication />}> */}
+			<Route path="/" element={<Authentication />}>
 				<Route path="/home" index element={<Home />} />
-			{/* </Route> */}
+			</Route>
 			<Route path="*" element={<NotFound />} />
 		</Routes>
 	);
